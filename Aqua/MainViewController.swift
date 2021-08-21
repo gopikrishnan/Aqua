b29925af27ac7b4bef625de23181d64991789e22
gopikrishnan <psgopikrishnan@gmail.com>
2021-08-21 20:45:35 +0530

    Fixed Sliders IBAction method

--- Aqua/MainViewController.swift
+++ Aqua/MainViewController.swift
@@ -8,43 +8,24 @@ class MainViewController: NSViewController {
     var reminder: Reminder = Reminder()
     var interval = 60;
     
-    
     required init(coder decoder: NSCoder) {
         super.init(coder: decoder)!
 
     }
-    
-    
-    
+
     @IBOutlet weak var slider: NSSlider!
-    
-//    self.slider.addTarget(self, action: #selector(sliderUpdates), for: UIControlEvents.valueChanged)
-   
-    func sliderChanged(slider: NSSlider) {
-        NSLog("slider value is changed %i",slider.intValue );
-        let sliderVal = slider.intValue;
-        if (sliderVal != 0) {
-            interval = interval + (Int(sliderVal)*15)
-        }
-        reminder.reset(intervalInSecs: Double(interval*60))
-        
-    }
-    @IBAction func sliderUpdates(slider: NSSlider) {
+    @IBAction func sliderUpdated(_ sender: NSSlider) {
         NSLog("slider value is changed %i",slider.intValue );
         let sliderVal = slider.intValue;
         if (sliderVal != 0) {
             interval = interval + (Int(sliderVal)*2)
         }
         reminder.reset(intervalInSecs: Double(interval*60))
-        
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         reminder.startTimer(intervalInSecs: Double(interval*60))
-
-//        slider.addTarget(self, action: #selector(sliderChanged( sender:)),for: .valueChanged)
-
     }
     override func viewDidAppear() {
         super.viewDidAppear()
